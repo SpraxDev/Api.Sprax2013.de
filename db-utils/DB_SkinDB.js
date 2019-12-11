c18b376d15a519a154f831fe74acaa451eec71df
Sprax2013 <Developer@sprax2013.de>
2019-12-11 08:53:33 +0100

    Fix err when a new user-agents provides a skin in multiple threads
    When a client connects multiple times with nearly no delay, an error can occurr when another connection already added the user-agent to the database

--- db-utils/DB_SkinDB.js
+++ db-utils/DB_SkinDB.js
@@ -113,7 +113,7 @@ module.exports = {
           }
 
           if (res.rowCount <= 0) {
-            return con.query(`INSERT INTO "QueuingAgents" ("Agent","Internal") VALUES ($1,$2) RETURNING "ID";`,
+            return con.query(`INSERT INTO "QueuingAgents" ("Agent","Internal") VALUES ($1,$2) ON CONFLICT ("Agent") DO NOTHING RETURNING "ID";`,
               [userAgent, internal], (err, res) => {
                 done();
 
