7439579b8b1710c9615ae79c8940c6220fb725bf
Christian Koop <developer@sprax2013.de>
2020-03-31 18:43:28 +0200

    SkinDB-Route: Reintroduce AI route for skin tagging

--- src/routes/skindb.ts
+++ src/routes/skindb.ts
@@ -1,31 +1,39 @@
+import { JSDOM } from 'jsdom';
+import canvas = require('canvas');
 import crypto = require('crypto');
 import fs = require('fs');
 import path = require('path');
 import request = require('request');
 
-// import canvas = require('canvas');
-// import { JSDOM } from 'jsdom';
-// import * as tmImage from '@teachablemachine/image';
+import tmImage = require('@teachablemachine/image');
 
 import { Router } from 'express';
 
 import { db } from '..';
 import { MinecraftUser, UserAgent, Skin, Cape, CapeType } from '../global';
-import { ErrorBuilder, restful, Image, setCaching } from '../utils';
+import { ErrorBuilder, restful, Image, setCaching, isNumber } from '../utils';
 import { getUserAgent } from './minecraft';
 
 const yggdrasilPublicKey = fs.readFileSync(path.join(__dirname, '..', '..', 'resources', 'yggdrasil_session_pubkey.pem'));
 
 /* AI */
-// (global as any).document = new JSDOM('<body></body>').window.document;
-// (global as any).fetch = require('node-fetch');
+(global as any).document = new JSDOM(`<body><script>document.body.appendChild(document.createElement('hr'));</script></body>`).window.document;
+(global as any).fetch = require('node-fetch');
+(global as any).document = new JSDOM('<body></body>').window.document;
+(global as any).fetch = require('node-fetch');
 
-// const AI_MODELS: { [key: string]: null | tmImage.CustomMobileNet } = {
-//   GENDER: null
-// };
-// (async () => {
-//   AI_MODELS.GENDER = await tmImage.load('https://sprax2013.de/ai/gender/model.json', 'https://sprax2013.de/ai/gender/metadata.json');
-// })();
+const AI_MODELS: { [key: string]: null | tmImage.CustomMobileNet | Error } = {
+  GENDER: null,
+  HUMAN: null
+};
+(async () => {
+  tmImage.load('https://internal.skindb.net/ai-models/gender/model.json', 'https://internal.skindb.net/ai-models/gender/metadata.json')
+    .then(obj => AI_MODELS.GENDER = obj)
+    .catch(err => { AI_MODELS.GENDER = err; console.error(err); });
+  tmImage.load('https://internal.skindb.net/ai-models/human/model.json', 'https://internal.skindb.net/ai-models/human/metadata.json')
+    .then(obj => AI_MODELS.HUMAN = obj)
+    .catch(err => { AI_MODELS.HUMAN = err; console.error(err); });
+})();
 
 /* Routes */
 const router = Router();
@@ -213,53 +221,55 @@ router.use('/cdn/capes/:id?', (req, res, next) => {
 //   });
 // });
 
-// router.all('/ai/:model?', async (req, res, next) => {
-//   restful(req, res, {
-//     get: () => {
-//       if (!req.params.model || !AI_MODELS.hasOwnProperty(req.params.model.toUpperCase())) return next(new ErrorBuilder().invalidParams('url', [{ param: 'model', condition: `Equal (ignore case) one of the following: ${Object.keys(AI_MODELS).join('", "')}` }]));
+router.all('/ai/:model?', async (req, res, next) => {
+  restful(req, res, {
+    get: () => {
+      if (!req.params.model || !AI_MODELS.hasOwnProperty(req.params.model.toUpperCase())) return next(new ErrorBuilder().invalidParams('url', [{ param: 'model', condition: `Equal (ignore case) one of the following: ${Object.keys(AI_MODELS).join('", "')}` }]));
 
-//       if (!req.query.skin) return next(new ErrorBuilder().invalidParams('query', [{ param: 'skin', condition: 'skin.length > 0' }]));
-//       if (!isNumber(req.query.skin)) return next(new ErrorBuilder().invalidParams('query', [{ param: 'skin', condition: 'Is numeric string (0-9)' }]));
+      if (!req.query.skin) return next(new ErrorBuilder().invalidParams('query', [{ param: 'skin', condition: 'skin.length > 0' }]));
+      if (!isNumber(req.query.skin)) return next(new ErrorBuilder().invalidParams('query', [{ param: 'skin', condition: 'Is numeric string (0-9)' }]));
 
-//       const skinID = req.query.skin;
-//       const model = AI_MODELS[req.params.model.toUpperCase()];
+      const skinID = req.query.skin;
+      const model = AI_MODELS[req.params.model.toUpperCase()];
 
-//       if (!model) {
-//         res.set('Retry-After', '2');
-//         return next(new ErrorBuilder().serviceUnavailable('This AI model is still being initialized'));
-//       }
+      if (!model) {
+        res.set('Retry-After', '2');
+        return next(new ErrorBuilder().serviceUnavailable('This AI model is still being initialized'));
+      } else if (model instanceof Error) {
+        return next(new ErrorBuilder().serviceUnavailable('The requested AI model failed to initialize'));
+      }
 
-//       db.getSkinImage(skinID, 'clean', (err, skin) => {
-//         if (err) return next(err);
-//         if (!skin) return next(new ErrorBuilder().notFound('Image for given skin'));
+      db.getSkinImage(skinID, 'clean', (err, skin) => {
+        if (err) return next(err);
+        if (!skin) return next(new ErrorBuilder().serverErr(`Could not find any image in db for skin (id=${skinID})`, true));
 
-//         getPrediction(model, skin, (err, result) => {
-//           if (err || !result) return next(err);
+        getPrediction(model, skin, (err, result) => {
+          if (err || !result) return next(err);
 
-//           return res.send(result);
-//         });
-//       });
-//     }
-//   });
-// });
+          return res.send(result);
+        });
+      });
+    }
+  });
+});
 
 /* Helper */
-// function getPrediction(model: tmImage.CustomMobileNet, data: Buffer, callback: (err: Error | null, result: null | { className: string, probability: number }[]) => void): void {
-//   const can = canvas.createCanvas(64, 64);
-//   const ctx = can.getContext('2d');
+function getPrediction(model: tmImage.CustomMobileNet, data: Buffer, callback: (err: Error | null, result: null | { className: string, probability: number }[]) => void): void {
+  const can = canvas.createCanvas(64, 64);
+  const ctx = can.getContext('2d');
 
-//   const img = new canvas.Image();
-//   img.onerror = err => callback(err, null);
-//   img.onload = async () => {
-//     ctx.drawImage(img, 0, 0, 64, 64);
+  const img = new canvas.Image();
+  img.onerror = err => callback(err, null);
+  img.onload = async () => {
+    ctx.drawImage(img, 0, 0, 64, 64);
 
-//     model.predict(can as any)
-//       .then(res => callback(null, res))
-//       .catch(err => callback(err, null));
-//   }
+    model.predict(can as any)
+      .then(res => callback(null, res))
+      .catch(err => callback(err, null));
+  }
 
-//   img.src = data;
-// }
+  img.src = data;
+}
 
 export function importByTexture(textureValue: string, textureSignature: string | null, userAgent: UserAgent, callback: (err: Error | null, skin: Skin | null, cape: Cape | null) => void): void {
   const texture = MinecraftUser.extractMinecraftProfileTextureProperty(textureValue);
