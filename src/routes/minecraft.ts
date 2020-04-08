279f37d1bdfc083a32defbc4198bede3eaa7c301
Christian Koop <developer@sprax2013.de>
2020-04-08 23:36:55 +0200

    Minecraft-Route: Whitelist cdn.skindb.net for skins

--- src/routes/minecraft.ts
+++ src/routes/minecraft.ts
@@ -70,12 +70,12 @@ setInterval(() => { rateLimitedNameHistory = 0 }, 120 * 1000);
 const SKIN_STEVE = fs.readFileSync(path.join(__dirname, '..', '..', 'resources', 'steve.png')),
   SKIN_ALEX = fs.readFileSync(path.join(__dirname, '..', '..', 'resources', 'steve.png'));
 
-const whitelistedSkinURLs = ['//textures.minecraft.net/texture/'];
+const whitelistedSkinURLs = ['//textures.minecraft.net/texture/', '//cdn.skindb.net/'];
 
 const router = Router();
 export const minecraftExpressRouter = router;
 
-// Turn :user into uuid (without hyphenes)
+// Turn :user into uuid (without hyphenes) or if :user == x-url check if query-param url is valid
 router.param('user', (req, _res, next, value, name) => {
   if (typeof value != 'string') return next(new ErrorBuilder().invalidParams('url', [{ param: 'user', condition: 'Is string' }]));
   value = value.trim();
