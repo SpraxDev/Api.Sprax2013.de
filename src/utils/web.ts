1e9ddc85d93f087355b2b5d2cbb35b61978c8eb5
Sprax <developer@sprax2013.de>
2020-06-09 03:22:28 +0200

    Rewrite outgoint HTTP request logic
    Sending a request to a server now is one method call that tries n times in case a 429 is returned or the connection timed out

--- src/utils/web.ts
+++ src/utils/web.ts
@@ -1,5 +1,6 @@
-import { jar, CookieJar, CoreOptions } from 'request';
+import { jar, CookieJar, CoreOptions, get, Response } from 'request';
 import { cfg } from '..';
+import { ApiError } from './utils';
 
 let lastProxy = 0;
 const proxies: { proxy: string, jar: CookieJar }[] =
@@ -7,8 +8,29 @@ const proxies: { proxy: string, jar: CookieJar }[] =
     [{ proxy: '', jar: jar() }] :
     cfg.proxies.map((val) => { return { proxy: val.length > 0 ? `http://${val}` : val, jar: jar() } });
 
+export async function getHttp(uri: string, triesLeft: number = 3): Promise<{ res: Response, body: Buffer }> {
+  return new Promise((resolve, reject) => {
+    get(uri, getRequestOptions(), (err, httpRes, httpBody: Buffer) => {
+      if (err) {
+        if (triesLeft > 0) {
+          if (err.code == 'ETIMEDOUT' || err.code == 'ECONNREFUSED' ||
+            httpRes.statusCode == 429 || httpRes.statusCode == 500 ||
+            httpRes.statusCode == 503 || httpRes.statusCode == 504) {
+            ApiError.log('Retrying with another proxy...', { uri, triesLeft });
+            return getHttp(uri, --triesLeft); // Retry with another proxy
+          }
+        }
+
+        return reject(err);
+      }
+
+      return resolve({ res: httpRes, body: httpBody });
+    });
+  });
+}
+
 export function getRequestOptions(): CoreOptions {
-  return getNextProxy();
+  return Object.assign(getNextProxy(), { encoding: null });
 }
 
 function getNextProxy(): { proxy: string, jar: CookieJar } {
