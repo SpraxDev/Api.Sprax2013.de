80e99c92ddaa2cd056b0e2868bd096f634fceec8
Sprax <Developer@sprax2013.de>
2020-06-10 17:39:50 +0200

    Rewrite outgoint HTTP request logic (#124)
    * Rewrite outgoint HTTP request logic
    
    Sending a request to a server now is one method call that tries n times in case a 429 is returned or the connection timed out
    
    * Fix duplicate lines
    
    * Fix 'Cannot read property of undefined'
    
    * Fix HTTP-Request not always retrying as intended
    
    * Make using the proxy pool optional
    
    * Optimize proxy-pool utilization
    
    * Proxy-Pool: Fetch more err codes
    
    * Improve Proxy-Pool error handling
    
    * Code cleanup
    
    * Don't report to Discord WebHook

--- src/utils/web.ts
+++ src/utils/web.ts
@@ -1,14 +1,39 @@
-import { jar, CookieJar, CoreOptions } from 'request';
+import { jar, CookieJar, CoreOptions, get, Response } from 'request';
 import { cfg } from '..';
+import { ApiError } from './utils';
 
 let lastProxy = 0;
 const proxies: { proxy: string, jar: CookieJar }[] =
   cfg.proxies.length == 0 ?
     [{ proxy: '', jar: jar() }] :
-    cfg.proxies.map((val) => { return { proxy: val.length > 0 ? `http://${val}` : val, jar: jar() } });
+    cfg.proxies.map((val) => { return { proxy: val.length > 0 ? `http://${val}` : val, jar: jar(), timeout: 1000 } });
 
-export function getRequestOptions(): CoreOptions {
-  return getNextProxy();
+export async function getHttp(uri: string, useProxy: boolean = true, triesLeft: number = 3): Promise<{ res: Response, body: Buffer }> {
+  return new Promise((resolve, reject) => {
+    get(uri, getRequestOptions(useProxy), (err, httpRes, httpBody: Buffer) => {
+      if (err || httpRes.statusCode == 429 || httpRes.statusCode == 500 ||
+        httpRes.statusCode == 503 || httpRes.statusCode == 504) {
+        if (triesLeft > 0) {
+          ApiError.log('Retrying with another proxy...', { uri, triesLeft }, true);
+          return getHttp(uri, useProxy, --triesLeft) // Retry with another proxy
+            .then(resolve)
+            .catch(reject);
+        } else if (triesLeft == 0 && useProxy) {
+          return getHttp(uri, false, --triesLeft) // One last try without proxy pool (my proxies are sometimes down for a couple of hours >:( - Patreons can help me with that c:)
+            .then(resolve)
+            .catch(reject);
+        }
+
+        return reject(err || new Error(`Outgoing request failed: ${uri}`));
+      }
+
+      return resolve({ res: httpRes, body: httpBody });
+    });
+  });
+}
+
+export function getRequestOptions(useProxy: boolean): CoreOptions {
+  return Object.assign(useProxy ? getNextProxy() : {}, { encoding: null });
 }
 
 function getNextProxy(): { proxy: string, jar: CookieJar } {
