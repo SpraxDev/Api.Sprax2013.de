755fef934aec9337775fcd8c507d9e4d3aaa78c0
Christian Koop <contact@sprax2013.de>
2024-05-11 01:13:16 +0200

    feat: implement first simple app bootstrapping with shutdown hooks
    Right now, it just calls SentrySdk#init() and registers shutdown hooks to
    gracefully shutdown the SentrySdk.

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,25 @@
 import 'reflect-metadata';
+import SentrySdk from './SentrySdk.js';
+
+await bootstrap();
+
+async function bootstrap(): Promise<void> {
+  await SentrySdk.init();
+  registerShutdownHooks();
+}
+
+function registerShutdownHooks(): void {
+  const handleShutdown = async () => {
+    console.log('Shutting down...');
+
+    await SentrySdk.shutdown();
+
+    console.log('Finished graceful shutdown.');
+    process.exit(0);
+  };
+
+  process.on('SIGTERM', handleShutdown);
+  process.on('SIGINT', handleShutdown);
+  process.on('SIGQUIT', handleShutdown);
+  process.on('SIGHUP', handleShutdown);
+}
