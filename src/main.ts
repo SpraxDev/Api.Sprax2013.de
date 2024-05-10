e6c5b7b22b7322b53ee6b541e672cdb5a5c8c8b7
Christian Koop <contact@sprax2013.de>
2024-05-11 01:52:35 +0200

    feat: implement simple fastify webserver error and not-found handlers

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,33 @@
 import 'reflect-metadata';
+import { container } from 'tsyringe';
+import { IS_PRODUCTION } from './constants.js';
 import SentrySdk from './SentrySdk.js';
+import FastifyWebServer from './webserver/FastifyWebServer.js';
+
+let webServer: FastifyWebServer | undefined;
 
 await bootstrap();
 
 async function bootstrap(): Promise<void> {
   await SentrySdk.init();
   registerShutdownHooks();
+
+  webServer = container.resolve(FastifyWebServer);
+  await webServer.listen('0.0.0.0', 8087);
+
+  console.log();
+  if (!IS_PRODUCTION) {
+    console.log('RUNNING IN DEVELOPMENT MODE');
+  }
+  console.log(`Application is ready to accept requests (http://127.0.0.1:8087/)`);
 }
 
 function registerShutdownHooks(): void {
   const handleShutdown = async () => {
     console.log('Shutting down...');
+
+    await webServer?.shutdown();
+    webServer = undefined;
 
     await SentrySdk.shutdown();
 
---
+++ src/webserver/FastifyWebServer.ts
@@ -0,0 +1,39 @@
+import Fastify, { type FastifyError, FastifyInstance, type FastifyReply, type FastifyRequest } from 'fastify';
+import { singleton } from 'tsyringe';
+import SentrySdk from '../SentrySdk.js';
+
+@singleton()
+export default class FastifyWebServer {
+  private readonly fastify: FastifyInstance;
+
+  constructor() {
+    this.fastify = Fastify({
+      ignoreDuplicateSlashes: true,
+      ignoreTrailingSlash: true,
+
+      trustProxy: false // TODO
+    });
+
+    this.fastify.setNotFoundHandler((_request, reply) => {
+      return reply
+        .code(404)
+        .type('application/json')
+        .send('{"error":"Not Found"}');
+    });
+    this.fastify.setErrorHandler((err: FastifyError, _req: FastifyRequest, reply: FastifyReply) => {
+      SentrySdk.logAndCaptureError(err);
+
+      return reply
+        .code(500)
+        .send('Internal Server Error');
+    });
+  }
+
+  async listen(host: string, port: number): Promise<void> {
+    await this.fastify.listen({ host, port });
+  }
+
+  async shutdown(): Promise<void> {
+    await this.fastify.close();
+  }
+}
