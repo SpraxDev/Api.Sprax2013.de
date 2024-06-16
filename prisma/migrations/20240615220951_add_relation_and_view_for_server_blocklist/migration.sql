47c8d3bd39ac8be1e131a3b5d33da757571ccc3c
Christian Koop <contact@sprax2013.de>
2024-06-16 19:55:41 +0200

    feat: implement persisting Minecraft server blocklist history in the db
    This allows me to see which hashes/servers were blocked
    at a given point in time.
    Maybe which server have been blocked multiple times or
    mostly just when a specific server started to get blocked.

---
+++ prisma/migrations/20240615220951_add_relation_and_view_for_server_blocklist/migration.sql
@@ -0,0 +1,28 @@
+-- CreateTable
+CREATE TABLE "server_blocklist_changes" (
+    "sha1" BYTEA NOT NULL,
+    "change_is_add" BOOLEAN NOT NULL,
+    "change_seen_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "server_blocklist_changes_pkey" PRIMARY KEY ("sha1","change_seen_at")
+);
+
+-- CreateIndex
+CREATE INDEX "server_blocklist_changes_sha1_change_seen_at_idx" ON "server_blocklist_changes"("sha1", "change_seen_at" DESC);
+
+-- CreateView
+CREATE MATERIALIZED VIEW "server_blocklist" AS
+    SELECT
+        x."sha1"
+    FROM (
+        SELECT DISTINCT ON ("sha1")
+            "sha1",
+            "change_is_add"
+        FROM
+            "server_blocklist_changes"
+        ORDER BY
+            "sha1",
+            "change_seen_at" DESC
+    ) as x
+    WHERE
+        "change_is_add" = true;
---
+++ src/minecraft/server/ServerBlocklistPersister.ts
@@ -0,0 +1,73 @@
+import { Prisma } from '@prisma/client';
+import { singleton } from 'tsyringe';
+import DatabaseClient from '../../database/DatabaseClient.js';
+
+// TODO: Call this every 5 minutes and fetch the blocklist for API responses from the database instead of Mojang's API
+@singleton()
+export default class ServerBlocklistPersister {
+  constructor(
+    private readonly databaseClient: DatabaseClient
+  ) {
+  }
+
+  async updateBlocklistInDatabase(blocklist: string[]): Promise<void> {
+    const blockListHashes = blocklist.map(v => Buffer.from(v, 'hex'));
+
+    await this.databaseClient.$transaction(async (transaction) => {
+      await transaction.$executeRaw`LOCK TABLE server_blocklist_changes IN EXCLUSIVE MODE NOWAIT;`;
+
+      const wroteAnyChanges1 = await this.updateHashesThatAreNoLongerBlocked(transaction, blockListHashes);
+      const wroteAnyChanges2 = await this.updateHashesThatAreNowBlocked(transaction, blockListHashes);
+      if (wroteAnyChanges1 || wroteAnyChanges2) {
+        await transaction.$executeRaw`REFRESH MATERIALIZED VIEW server_blocklist;`;
+      }
+    });
+  }
+
+  private async updateHashesThatAreNoLongerBlocked(transaction: Prisma.TransactionClient, blocklist: Buffer[]): Promise<boolean> {
+    const hashesNoLongerBlocked = await transaction.serverBlocklist.findMany({
+      where: {
+        sha1: {
+          notIn: blocklist
+        }
+      },
+      select: { sha1: true }
+    });
+    if (hashesNoLongerBlocked.length <= 0) {
+      return false;
+    }
+
+    await transaction.serverBlocklistChanges.createMany({
+      data: hashesNoLongerBlocked.map(hash => ({
+        sha1: hash.sha1,
+        changeIsAdd: false
+      }))
+    });
+    return true;
+  }
+
+  private async updateHashesThatAreNowBlocked(transaction: Prisma.TransactionClient, blocklist: Buffer[]): Promise<boolean> {
+    const knownBlockedHashesResult = await transaction.serverBlocklist.findMany({
+      where: {
+        sha1: { in: blocklist }
+      },
+      select: { sha1: true }
+    });
+    const knownBlockedHashes = new Set(knownBlockedHashesResult.map(hash => hash.sha1.toString('hex')));
+
+    let wroteAnyChanges = false;
+    for (const hashToBlock of blocklist) {
+      const hashAlreadyBlocked = knownBlockedHashes.has(hashToBlock.toString('hex'));
+      if (!hashAlreadyBlocked) {
+        await transaction.serverBlocklistChanges.create({
+          data: {
+            sha1: hashToBlock,
+            changeIsAdd: true
+          }
+        });
+        wroteAnyChanges = true;
+      }
+    }
+    return wroteAnyChanges;
+  }
+}
