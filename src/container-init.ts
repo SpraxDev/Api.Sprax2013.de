cdbb51b21d6200f6f776f8a029f438abf4639a42
Christian Koop <contact@sprax2013.de>
2024-07-13 11:24:42 +0200

    feat: Implement legacy v1 server blocklist endpoints

--- src/container-init.ts
+++ src/container-init.ts
@@ -1,7 +1,9 @@
 import 'reflect-metadata';
 import { container, Lifecycle } from 'tsyringe';
+import MinecraftV1Router from './webserver/routes/minecraft/MinecraftV1Router.js';
 import MinecraftV2Router from './webserver/routes/minecraft/MinecraftV2Router.js';
 import StatusRouter from './webserver/routes/StatusRouter.js';
 
 container.register('Router', { useClass: StatusRouter }, { lifecycle: Lifecycle.Singleton });
 container.register('Router', { useClass: MinecraftV2Router }, { lifecycle: Lifecycle.Singleton });
+container.register('Router', { useClass: MinecraftV1Router }, { lifecycle: Lifecycle.Singleton });
---
+++ src/webserver/routes/minecraft/MinecraftV1Router.ts
@@ -0,0 +1,86 @@
+import { FastifyInstance } from 'fastify';
+import { autoInjectable } from 'tsyringe';
+import ServerBlocklistService, {
+  InvalidHostError
+} from '../../../minecraft/server/blocklist/ServerBlocklistService.js';
+import FastifyWebServer from '../../FastifyWebServer.js';
+import Router from '../Router.js';
+
+@autoInjectable()
+export default class MinecraftV1Router implements Router {
+  constructor(
+    private readonly serverBlocklistService: ServerBlocklistService
+  ) {
+  }
+
+  register(server: FastifyInstance): void {
+    server.all('/mc/v1/servers/blocked', (request, reply): Promise<void> => {
+      return FastifyWebServer.handleRestfully(request, reply, {
+        get: async (): Promise<void> => {
+          const blocklist = await this.serverBlocklistService.provideBlocklist();
+          return reply
+            .header('Cache-Control', 'public, max-age=120, s-maxage=120')
+            .send(blocklist);
+        }
+      });
+    });
+
+    server.all('/mc/v1/servers/blocked/known', (request, reply): Promise<void> => {
+      return FastifyWebServer.handleRestfully(request, reply, {
+        get: async (): Promise<void> => {
+          const blocklist = await this.serverBlocklistService.provideBlocklistForKnownHosts();
+          const responseBody: { [key: string]: string } = {};
+          for (const listEntry of blocklist) {
+            if (listEntry.host != null) {
+              responseBody[listEntry.sha1.toString('hex')] = listEntry.host;
+            }
+          }
+
+          return reply
+            .header('Cache-Control', 'public, max-age=120, s-maxage=120')
+            .send(responseBody);
+        }
+      });
+    });
+
+    server.all('/mc/v1/servers/blocked/check', (request, reply): Promise<void> => {
+      return FastifyWebServer.handleRestfully(request, reply, {
+        get: async (): Promise<void> => {
+          const inputHost = (request.query as any).host;
+          if (typeof inputHost !== 'string' || inputHost.length <= 0) {
+            return reply
+              .status(400)
+              .send({
+                error: 'Bad Request',
+                message: 'Missing or invalid query parameters',
+                details: [{ param: 'host', 'condition': 'host.length > 0' }]
+              });
+          }
+
+          let blocklist;
+          try {
+            blocklist = await this.serverBlocklistService.checkBlocklist(inputHost);
+          } catch (err: any) {
+            if (err instanceof InvalidHostError) {
+              return reply
+                .status(400)
+                .send({
+                  error: 'Bad Request',
+                  message: 'Missing or invalid query parameters',
+                  details: [{ param: 'host', 'condition': 'A valid IPv4, IPv6 or domain' }]
+                });
+            }
+            throw err;
+          }
+          const responseBody: { [key: string]: boolean } = {};
+          for (const [host, isBlocked] of blocklist) {
+            responseBody[host] = isBlocked;
+          }
+          return reply
+            .header('Cache-Control', 'public, max-age=120, s-maxage=120')
+            .send(responseBody);
+        }
+      });
+    });
+  }
+}
