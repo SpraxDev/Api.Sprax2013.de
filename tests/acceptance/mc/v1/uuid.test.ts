f70e0353653cb0100d9a305587db90140baaaf5b
Christian Koop <contact@sprax2013.de>
2024-08-03 20:02:31 +0200

    test: introduce acceptance tests for legacy uuid endpoint

---
+++ tests/acceptance/mc/v1/uuid.test.ts
@@ -0,0 +1,78 @@
+import '../../../../src/container-init.js';
+import { FastifyInstance, type LightMyRequestResponse } from 'fastify';
+import { container } from 'tsyringe';
+import FastifyWebServer from '../../../../src/webserver/FastifyWebServer.js';
+import { EXISTING_MC_ID, EXISTING_MC_ID_WITH_HYPHENS, EXISTING_MC_NAME } from '../../test-constants.js';
+
+describe('/mc/v1/uuid/*', () => {
+  test('Expect 400 for empty username', async () => {
+    const response = await executeUuidRequest('');
+
+    expect(response.json()).toEqual({
+      error: 'Bad Request',
+      message: 'Missing or invalid url parameters',
+      details: [{ param: 'name', condition: 'name.length > 0' }]
+    });
+    expect(response.statusCode).toBe(400);
+  });
+
+  test.each([
+    [EXISTING_MC_NAME],
+    [EXISTING_MC_NAME.toUpperCase()]
+  ])('Expect 200: %j', async (username: string) => {
+    const response = await executeUuidRequest(username);
+
+    expect(response.json()).toEqual({
+      id: EXISTING_MC_ID,
+      name: EXISTING_MC_NAME
+    });
+    expect(response.statusCode).toBe(200);
+  });
+
+  test.each([
+    ['non-existing-$'],
+    ['a'.repeat(30)],
+    [EXISTING_MC_ID],
+    [EXISTING_MC_ID_WITH_HYPHENS],
+    [EXISTING_MC_ID_WITH_HYPHENS.toUpperCase()]
+  ])('Expect 404: %j', async (username: string) => {
+    const response = await executeUuidRequest(username);
+
+    expect(response.json()).toEqual({
+      error: 'Not Found',
+      message: 'UUID for given username'
+    });
+    expect(response.statusCode).toBe(404);
+  });
+
+  test('Expect 405 Method Not Allowed on POST', async () => {
+    const fastifyWebServer = container.resolve(FastifyWebServer);
+    const fastify = (fastifyWebServer as any).fastify as FastifyInstance;
+    const response = await fastify.inject({
+      method: 'POST',
+      url: '/mc/v1/uuid/' + EXISTING_MC_NAME
+    });
+
+    expect(response.statusCode).toBe(405);
+    expect(response.body).toBe('Method Not Allowed');
+    expect(response.headers['allow']).toBe('GET, HEAD');
+  });
+});
+
+async function executeUuidRequest(username: string, method: 'GET' | 'POST' = 'GET'): Promise<LightMyRequestResponse> {
+  const fastifyWebServer = container.resolve(FastifyWebServer);
+  const fastify = (fastifyWebServer as any).fastify as FastifyInstance;
+
+  const response = await fastify.inject({
+    method,
+    url: '/mc/v1/uuid/' + username
+  });
+
+  expect(response.headers['content-type']).toBe('application/json; charset=utf-8');
+
+  if (response.statusCode === 200 || response.statusCode === 404) {
+    expect(response.headers['cache-control']).toMatch(/^public, max-age=(60|300), s-maxage=\1$/);
+  }
+
+  return response;
+}
---
+++ tests/acceptance/test-constants.ts
@@ -0,0 +1,3 @@
+export const EXISTING_MC_NAME = 'SpraxDev';
+export const EXISTING_MC_ID = '955e4cf6411c40d1a1765bc8e03a8a9a';
+export const EXISTING_MC_ID_WITH_HYPHENS = '955e4cf6-411c-40d1-a176-5bc8e03a8a9a';
