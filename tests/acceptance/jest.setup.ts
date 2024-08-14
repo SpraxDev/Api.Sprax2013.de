102843e13ca4ea4b5dc83a789def932fc660f86a
Christian Koop <contact@sprax2013.de>
2024-08-14 20:42:15 +0200

    test: Set test timeout for acceptance tests from 5s to 8s
    Sometimes, those tests might take a bit longer and that's okay.
    I hope 8 seconds is enough.

--- tests/acceptance/jest.setup.ts
+++ tests/acceptance/jest.setup.ts
@@ -1,8 +1,11 @@
 import '../../src/container-init.js';
+import { jest } from '@jest/globals';
 import { mockDeep } from 'jest-mock-extended';
 import { container } from 'tsyringe';
 import DatabaseClient from '../../src/database/DatabaseClient.js';
 import HttpClient from '../../src/http/HttpClient.js';
+
+jest.setTimeout(8_000);
 
 beforeAll(async () => {
   (HttpClient as any).DEBUG_LOGGING = false;
