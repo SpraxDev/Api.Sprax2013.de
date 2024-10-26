57b0bfd9bfe886b5404aae1be7f4ea0800e5407e
Christian Koop <contact@sprax2013.de>
2024-10-26 13:42:42 +0200

    test: add unit tests for ProfilePersister

---
+++ tests/unit/minecraft/persistance/base/ProfilePersister.test.ts
@@ -0,0 +1,55 @@
+import { jest } from '@jest/globals';
+import { DeepMockProxy } from 'jest-mock-extended';
+import DatabaseClient from '../../../../../src/database/DatabaseClient.js';
+import ProfilePersister from '../../../../../src/minecraft/persistance/base/ProfilePersister.js';
+import { EXISTING_MC_ID, EXISTING_MC_NAME, EXISTING_MC_PROFILE_RESPONSE } from '../../../../test-constants.js';
+import { createStrictDeepMock } from '../../../../test-helpers.js';
+
+let databaseClient: DeepMockProxy<DatabaseClient>;
+let profilePersister: ProfilePersister;
+
+beforeEach(() => {
+  databaseClient = createStrictDeepMock<DatabaseClient>({
+    profile: {
+      upsert: jest.fn<any>().mockResolvedValue(undefined),
+      update: jest.fn<any>().mockResolvedValue(undefined)
+    }
+  });
+
+  profilePersister = new ProfilePersister(databaseClient);
+});
+
+describe('#ProfilePersister', () => {
+  test('#persist', async () => {
+    await expect(profilePersister.persist(EXISTING_MC_PROFILE_RESPONSE)).resolves.toBeUndefined();
+
+    expect(databaseClient.profile.upsert).toHaveBeenCalledTimes(1);
+    expect(databaseClient.profile.upsert).toHaveBeenCalledWith(expect.objectContaining({
+      where: { id: EXISTING_MC_ID },
+      create: {
+        id: EXISTING_MC_ID,
+        nameLowercase: EXISTING_MC_NAME.toLowerCase(),
+        raw: EXISTING_MC_PROFILE_RESPONSE
+      },
+      update: {
+        nameLowercase: EXISTING_MC_NAME.toLowerCase(),
+        raw: EXISTING_MC_PROFILE_RESPONSE,
+        deleted: false
+      }
+    }));
+
+    expect(databaseClient.profile.update).toHaveBeenCalledTimes(0);
+  });
+
+  test('#persistProfileAsDeleted', async () => {
+    await expect(profilePersister.persistProfileAsDeleted(EXISTING_MC_ID)).resolves.toBeUndefined();
+
+    expect(databaseClient.profile.update).toHaveBeenCalledTimes(1);
+    expect(databaseClient.profile.update).toHaveBeenCalledWith(expect.objectContaining({
+      where: { id: EXISTING_MC_ID },
+      data: { deleted: true }
+    }));
+
+    expect(databaseClient.profile.upsert).toHaveBeenCalledTimes(0);
+  });
+});
